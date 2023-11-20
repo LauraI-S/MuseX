c92a683026b404d1742d840a3637a7cff6fd3a40
LauraI-S <laura.ikvaiszabo@gmail.com>
2023-11-20 21:39:56 +0100

    getting closer to connect to my database, but still facing issues. Tomorrow I´m going to ask my mento what this error-warning might cause:" DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead."

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/testRoute.js";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+const router = express.Router();
+
+// loading .env file
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,10 +16,26 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
 
 const port = process.env.PORT || 5001;
+
+const DBConnection = async () => {
+  console.log("process.env.DB :>> ", process.env.DB);
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("connection to MONGODB established");
+  } catch (error) {
+    console.log("error :>> ", object);
+  }
+};
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
 
-app.use("/myApi", router);
+app.use("/api", router);
+router.get("/test", (req, res) => {
+  res.json({
+    message: "test route",
+  });
+});
--- server/routes/testRoute.js
+++ server/routes/testRoute.js
@@ -5,4 +5,6 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.send({ msg: "Test route." });
 });
+console.log(object);
 export default router;
+afsg;
