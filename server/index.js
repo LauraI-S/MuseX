74c2f1b5ba7ecaab59bbae777557f6d2d7285594
LauraI-S <laura.ikvaiszabo@gmail.com>
2023-11-21 11:07:33 +0100

    First commit

--- server/index.js
+++ server/index.js
@@ -1,41 +1,61 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/testRoute.js";
+import colors from "colors";
+// import router from "./routes/testRoute.js";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import musicianRoute from "../server/routes/musicianRoute.js";
 
+dotenv.config();
 const router = express.Router();
 
-// loading .env file
-dotenv.config();
-
 const app = express();
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
 
 const port = process.env.PORT || 5001;
 
+// loading .env file
+
 const DBConnection = async () => {
-  console.log("process.env.DB :>> ", process.env.DB);
+  console.log("process.env.DB :>> ", process.env.MONGO_URI);
+
   try {
-    await mongoose.connect(process.env.DB);
-    console.log("connection to MONGODB established");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connection to MONGODB established".bgGreen);
   } catch (error) {
-    console.log("error :>> ", object);
+    console.log("error connection to MONGODB:>> ".red, error);
   }
 };
-app.listen(port, () => {
-  console.log("Server is running on " + port + "port");
-});
+DBConnection();
 
+const addMiddlewares = () => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+};
 app.use("/api", router);
+app.use("/api", musicianRoute);
 router.get("/test", (req, res) => {
   res.json({
-    message: "test route",
+    message: "this is my test route",
   });
 });
+
+app.listen(port, () => {
+  console.log("Server is running on ".rainbow + port + " port".rainbow);
+  // console.log("hello :>> ");
+});
+
+//!IIFE  (Immediately Invoked Function Expression)
+
+const serverController = () => {};
+
+(async function controller() {
+  await DBConnection();
+  addMiddlewares();
+  addRoutes();
+  startServer();
+})();
--- server/routes/testRoute.js
+++ server/routes/testRoute.js
@@ -1,10 +1,9 @@
-import express from "express";
+// import express from "express";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send({ msg: "Test route." });
-});
-console.log(object);
-export default router;
-afsg;
+// router.get("/test", (req, res) => {
+//   res.send({ msg: "Test route." });
+// });
+// console.log(object);
+// export default router;
