b76fde728d4c6c109a12f969a8674a978a4160bf
LauraI-S <laura.ikvaiszabo@gmail.com>
2023-11-29 15:14:29 +0100

    having my signup-page on the frontend almost  working

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,81 +1,83 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import '../styles/SignUp.css'; // Import a CSS file for custom styling
 
 function SignUp() {
-  const [displayName, setDisplayName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordType, setPasswordType] = useState('password');
+  const [newUser, setNewUser] = useState<User>({
+    userName: "",
+    email: "",
+    password: "",
+    userImage: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleDisplayNameChange = (e) => {
-    setDisplayName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleSignupInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    //!the following line basically combines the new user-information with existing information
+    //!...:spread-operator keeps track of the previously stored information
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+    console.log('e.target.name :>> ', e.target.name);
+    console.log('e.target.value :>> ', e.target.value);
   };
 
   const togglePasswordVisibility = () => {
-    setPasswordType((prevType) => (prevType === 'password' ? 'text' : 'password'));
+    setShowPassword(!showPassword);
   };
 
+  //!preventing new page-reload by default, console-logging my new user
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log('newUser :>> ', newUser);
     // Add your sign-up logic here
   };
-
   return (
-    <div className="container">
-      <h1 className="text-center">Sign Up</h1>
+    <div className="container fancy-container">
+
       <br />
-      <div className="registration-container">
+      <div className="registration-container fancy-form-container">
         <h1 className="text-center">SIGN UP</h1>
-        <Form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <p className="text-center">Fill in the information below to sign up:</p>
-          <Form.Group controlId="username">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
               type="text"
-              placeholder="Choose username..."
-              value={displayName}
-              onChange={handleDisplayNameChange}
+              name="username"
+              onChange={handleSignupInputChange}
+              className="form-control"
             />
-          </Form.Group>
-          <Form.Group controlId="email">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
               type="text"
               placeholder="Enter e-mail..."
-              value={email}
-              onChange={handleEmailChange}
+              name="email"
+              onChange={handleSignupInputChange}
+              className="form-control"
               required
             />
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type={passwordType}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type={showPassword ? 'hide' : 'password'}
               placeholder="Enter password..."
-              value={password}
-              onChange={handlePasswordChange}
+              name="password"
+              onChange={handleSignupInputChange}
+              className="form-control"
               required
             />
-            <Form.Text
-              className="text-muted"
-              style={{ cursor: 'pointer' }}
+            <div
+              className="password-toggle"
               onClick={togglePasswordVisibility}
             >
-              {passwordType === 'password' ? 'Show' : 'Hide'} password
-            </Form.Text>
-          </Form.Group>
-          <Button variant="primary" type="submit" block>
+              {showPassword ? 'Rather hide' : 'Show me that'} password
+            </div>
+          </div>
+          <Button type="submit" className="btn-primary btn-block">
             Sign up
           </Button>
-        </Form>
+        </form>
       </div>
       <p className="text-center">
         Already have an account?{' '}
@@ -83,20 +85,7 @@ function SignUp() {
           Log in!
         </a>
       </p>
-      <Form>
-        <Form.Group controlId="newsletterCheckbox" className="ckeckbox-box">
-          <Form.Check
-            type="checkbox"
-            label={
-              <span className="checkbox-label">
-                Yes, please send me tasty emails! I'd like to receive news and exclusive offers
-                from the Cook`cinelle. By signing up, you agree to our Terms of Use. Learn how we
-                collect, use, and share your data in our Privacy Policy.
-              </span>
-            }
-          />
-        </Form.Group>
-      </Form>
+      {/* Your checkbox can be added here */}
     </div>
   );
 }
